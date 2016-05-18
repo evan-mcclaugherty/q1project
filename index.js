fcc231121cc61e761496581a037b178496b2aa1a
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-05-18 08:07:44 -0600

    reverted back to full loading carousel at once

--- index.js
+++ index.js
@@ -30,67 +30,31 @@ class Brewery {
 
 $(document).ready(
     function() {
-        $.get("https://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations?locality=Denver&key=fbb4282721faf956ef728ec873e1cdc8").done(function(obj) {
-            obj.data.forEach(el => {
-                if (el.website !== '') {
-                    breweryList.push(new Brewery(el));
-                }
-            });
-        }).done(
+        $.get("http://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations?locality=Denver&key=fbb4282721faf956ef728ec873e1cdc8").done(
+            function(obj) {
+                obj.data.forEach(el => {
+                    if (el.website !== '') {
+                        breweryList.push(new Brewery(el));
+                    }
+                });
+            }).done(
             function() {
                 let carousel = $('#ca-item').html();
                 let template = Handlebars.compile(carousel);
-
-                // ITERATOR
-                function makeIterator(breweryList) {
-                    var nextIndex = 0;
-                    return {
-                        next: function() {
-                            return nextIndex < breweryList.length ? {
-                                value: breweryList[nextIndex++],
-                                done: false
-                            } : {
-                                done: true
-                            };
-                        }
-                    }
-                }
-                var brewIterator = makeIterator(breweryList);
-
-                // GENERATOR
                 let counter = 1;
-
-                function* carouselMaker() {
-                        let next = brewIterator.next();
-                        while (!next.done) {
-                            let nextBrewery = next.value;
-                            let carouselData = template({
-                                title: nextBrewery.name,
-                                details: nextBrewery.description,
-                                description: nextBrewery.description
-                            });
-                            $('.ca-wrapper').append(`<div id="no${counter}" class="ca-item ca-item-${counter}"></div>`);
-                            $(`#no${counter}`).html(carouselData);
-                            $(`#no${counter} .ca-icon`).css('background-image', `url('${nextBrewery.medium}')`);
-                            counter++;
-                            yield
-                            next = brewIterator.next();
-                        }
-                    }
-                    // END GENERATOR
-
-                var carouselGenerator = carouselMaker();
-                carouselGenerator.next();
-                carouselGenerator.next();
-                carouselGenerator.next();
-                carouselGenerator.next();
-                $('#ca-container').contentcarousel();
-                $('span.ca-nav-next').click(
-                    function() {
-                        console.log("WTF????");
-                        carouselGenerator.next();
-                        $('#ca-container').contentcarousel();
+                for (let brew of breweryList) {
+                    console.log(brew.name)
+                    let carouselData = template({
+                        title: brew.name,
+                        details: brew.description,
+                        description: brew.description
                     });
+                    $('.ca-wrapper').append(`<div id="no${counter}" class="ca-item ca-item-${counter}"></div>`);
+                    $(`#no${counter}`).html(carouselData);
+                    $(`#no${counter} .ca-icon`).css('background-image', `url('${brew.medium}')`);
+                    counter++;
+                }
+                $('#ca-container').contentcarousel();
             }
-        )
-    })
+        );
+    });
